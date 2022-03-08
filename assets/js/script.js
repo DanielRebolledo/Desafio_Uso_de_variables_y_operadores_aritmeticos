dda2b28b101956c8be3f2f1af0c061b596796701
DanielRebolledo <danielrebolledo.182@gmail.com>
2022-03-08 01:46:50 -0300

    segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 //Calculadora
 
 function resultado(){
-    var num1 = parseInt(document.getElementById("numero1").value);
-    var num2 = parseInt(document.getElementById("numero2").value);  
-    var suma   = Math.floor(num1 + num2);
-    var resta  = Math.floor(num1 - num2);
-    var multi  = Math.floor(num1 * num2);
-    var divi   = Math.floor(num1 / num2);
-    var modulo = Math.floor(num1 % num2);
+    let num1 = parseInt(document.getElementById("numero1").value);
+    let num2 = parseInt(document.getElementById("numero2").value);  
+    let suma   = Math.floor(num1 + num2);
+    let resta  = Math.floor(num1 - num2);
+    let multi  = Math.floor(num1 * num2);
+    let divi   = Math.floor(num1 / num2);
+    let modulo = Math.floor(num1 % num2);
 
     
     document.getElementById("resultado").innerHTML= 
@@ -27,7 +27,7 @@ document.getElementById("button").onclick= function(){
 //Convertor de Temperatura
 
 function convertir(){
-    var temp = parseInt(document.getElementById("temperatura").value);
+    let temp = parseInt(document.getElementById("temperatura").value);
     const kelvin      = Math.floor (temp + 273.15);
     const fahrenheit  = Math.floor (temp * 9/5 + 32);
 
@@ -66,14 +66,14 @@ function calcularDias(){
 //Calcular Promedio
 
 function promedio(){
-    var nota1 = parseInt(document.getElementById("nota1").value);
-    var nota2 = parseInt(document.getElementById("nota2").value);
-    var nota3 = parseInt(document.getElementById("nota3").value);
-    var nota4 = parseInt(document.getElementById("nota4").value);
-    var nota5 = parseInt(document.getElementById("nota5").value);
+    let nota1 = parseInt(document.getElementById("nota1").value);
+    let nota2 = parseInt(document.getElementById("nota2").value);
+    let nota3 = parseInt(document.getElementById("nota3").value);
+    let nota4 = parseInt(document.getElementById("nota4").value);
+    let nota5 = parseInt(document.getElementById("nota5").value);
 
-    var sumaNotas = nota1 + nota2 + nota3 + nota4 + nota5;
-    var promedio  = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+    let sumaNotas = nota1 + nota2 + nota3 + nota4 + nota5;
+    let promedio  = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
     
     document.getElementById("promedio").innerHTML=
     
